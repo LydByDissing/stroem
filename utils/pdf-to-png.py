f634527ee25dcd936898c19b645c83d60e488441
Tue S. Dissing <tue@somersault.dk>
2021-12-22 22:36:22 +0100

    adding support for in/out parameters

--- utils/pdf-to-png.py
+++ utils/pdf-to-png.py
@@ -1,8 +1,30 @@
-import fitz
+#!/usr/local/bin/python3
 
-pdffile = "back-panel-drawing.pdf"
-doc = fitz.open(pdffile)
-page = doc.load_page(0)  # number of page
-pix = page.get_pixmap()
-output = "back-panel-drawing.png"
-pix.save(output)
+import fitz
+import sys, getopt
+import os
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    doc = fitz.open(inputfile)
+    page = doc.load_page(0)  # number of page
+    pix = page.get_pixmap()
+    pix.save(outputfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
